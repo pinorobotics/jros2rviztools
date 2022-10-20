798b66a97dc57a33f14eb169624203f3251e85b4
aeon <aeon_flux@eclipso.ch>
2022-10-20 00:34:42 +0000

    Adding test as in the documentation

--- jros2rviztools.tests/src/test/java/pinorobotics/jros2rviztools/tests/integration/JRos2RvizToolsIntegrationTests.java
+++ jros2rviztools.tests/src/test/java/pinorobotics/jros2rviztools/tests/integration/JRos2RvizToolsIntegrationTests.java
@@ -20,16 +20,11 @@ package pinorobotics.jros2rviztools.tests.integration;
 import static pinorobotics.jros2rviztools.tests.integration.TestConstants.RVIZ_MARKER_TOPIC;
 
 import id.jros2client.JRos2ClientFactory;
-import id.jrosclient.JRosClient;
 import id.xfunction.logging.XLogger;
-import java.net.MalformedURLException;
 import java.nio.file.Paths;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pinorobotics.jros2rviztools.JRos2RvizToolsFactory;
-import pinorobotics.jrosrviztools.JRosRvizTools;
 import pinorobotics.jrosrviztools.entities.Color;
 import pinorobotics.jrosrviztools.entities.MarkerType;
 import pinorobotics.jrosrviztools.entities.Point;
@@ -41,31 +36,19 @@ import pinorobotics.jrosrviztools.entities.Scales;
  */
 public class JRos2RvizToolsIntegrationTests {
 
-    private static final JRos2ClientFactory clientFactory = new JRos2ClientFactory();
-    private static final JRos2RvizToolsFactory toolsFactory = new JRos2RvizToolsFactory();
-    private JRosClient client;
-    private JRosRvizTools rvizTools;
-
     @BeforeAll
     public static void setupAll() {
         XLogger.load("jros2rviztools-test.properties");
     }
 
-    @BeforeEach
-    public void setup() throws MalformedURLException {
-        client = clientFactory.createClient();
-        rvizTools = toolsFactory.createJRosRvizTools(client, "map", RVIZ_MARKER_TOPIC);
-    }
-
-    @AfterEach
-    public void clean() throws Exception {
-        rvizTools.close();
-        client.close();
-    }
-
     @Test
-    public void test_all() throws Exception {
-        try (var commands = new Ros2Commands()) {
+    public void test_example_from_documentation() throws Exception {
+        var clientFactory = new JRos2ClientFactory();
+        var rvizToolsFactory = new JRos2RvizToolsFactory();
+        try (var commands = new Ros2Commands();
+                var client = clientFactory.createClient();
+                var rvizTools =
+                        rvizToolsFactory.createJRosRvizTools(client, "map", RVIZ_MARKER_TOPIC)) {
             var rviz = commands.runRviz(Paths.get("").toAbsolutePath().resolve("test.rviz"));
             rvizTools.publishText(
                     Color.RED, Scales.XLARGE, new Pose(new Point(0, 0, 1)), "Hello from Java");
